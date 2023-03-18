60fb445e7dd9c1c4155090a45782cb524c334263
Terrence Chung <terrencechung35@gmail.com>
2023-03-18 15:45:34 -0700

    finished seeding

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
 from models import db, Scientist, Planet, Mission
 
@@ -14,12 +15,17 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
 
-@app.route('/')
-def index():
-    response = make_response(
-        {"message": "Hello Scientists!"}
-    )
+class Scientists(Resource):
+    pass
+api.add_resource(Scientists,'/scientists')
+class Planets(Resource):
+    pass
+api.add_resource(Planets,'/planets')
+class Missions(Resource):
+    pass
+api.add_resource(Missions,'/missions')
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -3,6 +3,7 @@ from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy import func
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -15,15 +16,69 @@ class Mission(db.Model, SerializerMixin):
     __tablename__ = 'missions'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    scientist_id = db.Column(db.Integer, db.ForeignKey('scientists.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
 
+    scientist=db.relationship('Scientist', back_populates='missions')
+    planet=db.relationship('Planet', back_populates='missions')
+
+    @validates('name')
+    def validate_name(self,key,value):
+        if not value:
+            raise ValueError('Must have a name')
+        return value
+
+    @validates('scientist_id')
+    def validate_scientist(self,key,value):
+        if not value:
+            raise ValueError('Must have a scientist')
+        if Mission.query.filter_by(scientist_id=value, planet_id=self.planet_id).first():
+            raise ValueError('Scientist already joined this mission')
+        return value
+
+    @validates('planet_id')
+    def validate_planet(self,key,value):
+        if not value:
+            raise ValueError('Must have a planet')
+        return value
 
 class Scientist(db.Model, SerializerMixin):
     __tablename__ = 'scientists'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    field_of_study = db.Column(db.String)
+    avatar = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    missions = db.relationship('Mission',back_populates='scientist')
+    planets = association_proxy('missions', 'planet')
+
+    @validates('name')
+    def validate_name(self,key,value):
+        if not value:
+            raise ValueError('Must have a name')
+        return value
+    @validates('field_of_study')
+    def validate_field_of_study(self,key,value):
+        if not value:
+            raise ValueError('Must have a field of study')
+        return value
 
 
 class Planet(db.Model, SerializerMixin):
     __tablename__ = 'planets'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    distance_from_earth = db.Column(db.String)
+    nearest_star = db.Column(db.String)
+    image = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+
+    missions = db.relationship('Mission', back_populates='planet')
+    scientists = association_proxy('missions', 'scientist')
